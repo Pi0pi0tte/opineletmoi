9746e1222eed09c3cd03ebde6ab1e93701ed9eb6
Pi0pi0tte <laurent.ardevol@gmail.com>
2024-04-26 21:55:13 +0200

    correction du formulaire d'ajout d'un modèle

--- action.php
+++ action.php
@@ -1,16 +1,16 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "opineletmoi";
-
     if($_SERVER['REQUEST_METHOD'] == "POST" and array_key_exists('add_modele', $_POST))
     {
-        func();
-    }
-    function func()
-    {
+    //     func();
+    // }
+    // function func()
+    // {
         echo "This is Add Modele Button that is selected"; 
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "opineletmoi";
             
         // Create connection
         $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -19,18 +19,27 @@
         die("Connection failed: " . mysqli_connect_error());
         }
 
-        $sql = "INSERT INTO MODELES (designation, taille, id_serie, annee, nb_exemplaire, numerote, texte) VALUES (".$_POST['designation']", $_POST['taille'], $_POST['serie'], $_POST['annee'], $_POST['nbexemplaire'], $_POST['numerote'], ".$_POST['texte']")";
-        $result = mysqli_query($conn, $sql);
+        $sql = "INSERT INTO MODELES (designation, taille, id_serie, annee, nb_exemplaire, numerote, texte) 
+        VALUES ('$_POST[designation]', $_POST[taille], $_POST[serie], $_POST[annee], $_POST[nbexemplaire], $_POST[numerote], '$_POST[texte]')";
+        
+        try {
+            mysqli_query($conn, $sql);
+        } catch (Exception $e) {
+            echo "<br>Erreur : " . $sql . "<br>" . mysqli_error($conn);
+            return;
+        } 
 
-        if (mysqli_query($conn, $sql)) {
-            echo "Nouveau enregistrement créé avec succès";
-        } else {
-                echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
-        }
+        echo "<br>Nouveau enregistrement créé avec succès";
+
+        // $result = mysqli_query($conn, $sql);
+        // if (mysqli_query($conn, $sql)) {
+        //     echo "Nouveau enregistrement créé avec succès";
+        // } else {
+        //         echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
+        // }
     
     
     }
 
 
     mysqli_close($conn);
-?>
